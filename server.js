67a0cf8d28912c3c4685c10ff6431cee19d66c52
kbbushman <k.bushman@yahoo.com>
2019-05-06 13:33:57 -0700

    Fixes whitelist typo

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://wayfarer-react.herokuapp.com/'], // string or array
+  origin: ['http://localhost:3000', 'https://wayfarer-react.herokuapp.com'], // string or array
   credentials: true, // This allows the session cookie to be sent back and forth
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
