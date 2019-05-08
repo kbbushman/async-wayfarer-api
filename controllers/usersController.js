399921a80f63836d16da65a6881db8999ca73c5c
kbbushman <k.bushman@yahoo.com>
2019-05-08 08:03:11 -0700

    Removes unused code

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,39 +6,21 @@ const db = require('../models');
 
 // GET User Show Route
 router.get('/:userId', async (req, res) => {
-  // if (req.session.currentUser && req.session.currentUser.email !== req.params.email) {
-  //   return res.status(401).json({status: 401, error: 'Unauthorized.'});
-  // }
-
   try {
-    const user = await db.User.findById(req.params.userId, {password: 0, email: 0, __v: 0});
-    const posts = await db.Post.find({userId: req.params.userId})
-      // .populate('userId')
-      .populate({path: 'userId', select: 'name'})
+    const user = await db.User.findById(req.params.userId, {password: 0, __v: 0});
+    if (!user) return res.status(404).json({status: 404, error: 'User not found'});
+
+    const userPosts = await db.Post.find({user_id: req.params.userId})
+      .populate({ path: 'user_id', select: 'name' })
       .exec();
-    if (!user) {
-      res.status(404).json({status: 404, error: 'User not found'});
-    }
-    
-    return res.json({user, posts});
-    
-  } catch (err) {
-    res.status(500).json({status: 500, error: 'Something went wrong. Please try again'});
+
+    res.json({user, userPosts});
+
+  } catch(err) {
+    console.log(err);
+    return res.status(500).json({status: 500, error: 'Something went wrong. Please try again'});
   }
 });
-
-
-// GET User Posts Index Route
-// router.get('/:userId/posts', async (req, res) => {
-//   try {
-//     const data = await db.Post.find({userId: req.params.userId})
-//       .populate('userId')
-//       .exec();
-//     res.json(data);
-//   } catch (err) {
-//     res.status(500).json({status: 500, error: 'Something went wrong. Please try again'});
-//   }
-// })
 
 
 module.exports = router;
